8951e1cf6f580e67a79ba8c0c2b3e2a9771d221d
Adam Graham <bythegram@gmail.com>
2018-10-13 06:37:07 -0400

    moving service to new file and using observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-
-let DEFAULT = ['1234567890', 'qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+import { Observable } from 'rxjs';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'Limelight Encoder';
   form: FormGroup;
-  output: string;
+  output$: Observable<string>;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private appService: AppService) {
     const defaultEncode = 'V, H, 2, H, V, -2';
     const defaultText = 'Lorem ipsum dolor sit amet';
     this.form = this.fb.group({
       'encode': new FormControl(defaultEncode, Validators.required),
       'text': new FormControl(defaultText, Validators.required)
-    })
+    });
   }
 
   /**
@@ -28,118 +29,7 @@ export class AppComponent {
   handleFormSubmit() {
     const encode = this.form.get('encode').value;
     const text = this.form.get('text').value;
-    const defaultList = DEFAULT.join('')
-
-    const encodedString = this.handleEncoding(encode);
-    this.output = this.mapText(encodedString, text, defaultList);
+    this.output$ = this.appService.encodeData(encode, text);
   }
 
-  /**
-   * Handle encode arguments
-   */
-  handleEncoding(encode) {
-    const encodedArray = encode.split(',');
-    let list = DEFAULT;
-    encodedArray.map(en => {
-      const trimmed = en.trim().toUpperCase();
-      /**
-       * If it is a number
-       */
-      if (Number(trimmed)) {
-        switch ((Number(trimmed) >= 0) ? 1 : -1) {
-          case 1: {
-            console.log('Positive Shift');
-            list = this.shiftRight(Number(trimmed), list)
-
-            break
-          }
-          case -1: {
-            console.log('Negative Shift');
-            list = this.shiftLeft(Number(trimmed), list)
-
-            break
-          }
-        }
-        return false;
-      }
-
-      /**
-       * Handle Horizontal/Vertical Flips
-       */
-      switch (trimmed) {
-        case ('H'): {
-          console.log('Horizontal Flip');
-          list = this.horizontalFlip(list)
-          break
-        }
-        case ('V'): {
-          console.log('Vertical Flip');
-          list = this.verticalFilp(list)
-          break
-        }
-        default: {
-          console.warn('Invalid Code:', trimmed);
-        }
-      }
-    })
-    // Final encoded string
-    return list.join('');
-  }
-
-  /**
-   * Map Text To Encodeing
-   */
-  mapText(encodedString, text, defaultList) {
-    const mappedArray = text.split('').map(t => {
-      const place = defaultList.indexOf(t.toLowerCase())
-      if (place < 0) {
-        // Invalid character
-        return t;
-      }
-      // Encoded character
-      return encodedString[place];
-    })
-    return mappedArray.join('');
-  }
-
-  /**
-   * Shift Left - TODO: still needs fixing
-   */
-  shiftLeft(places, list) {
-    const shift = places *= -1;
-    return list.map(d => {
-      const asArray = d.split('');
-      asArray.unshift.apply(asArray, asArray.splice(0, shift));
-
-      return asArray.join('')
-    })
-  }
-
-  /**
-   * Shift Right
-   */
-  shiftRight(places, list) {
-    return list.map(d => {
-      const asArray = d.split('');
-      asArray.push.apply(asArray, asArray.splice(0, places));
-
-      return asArray.join('')
-    })
-  }
-
-  /**
-   * Horizontal Flip
-   */
-  horizontalFlip(list) {
-    return list.map(d => {
-      return d.split('').reverse().join('')
-    });
-  }
-
-  /**
-   * Vertical Flip
-   */
-  verticalFilp(list) {
-    return list.reverse()
-  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+let DEFAULT = ['1234567890', 'qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+
+@Injectable()
+export class AppService {
+    encodedString: string;
+  constructor() { }
+
+  encodeData(encodeKey: string, stringToEncode: string): Observable<string> {
+    const defaultList = DEFAULT.join('')
+
+    const encodedString = this.handleEncoding(encodeKey);
+    return of(this.mapText(encodedString, stringToEncode, defaultList));
+  }
+  /**
+   * Handle encode arguments
+   */
+  handleEncoding(encode) {
+    const encodedArray = encode.split(',');
+    let list = DEFAULT;
+    encodedArray.map(en => {
+      const trimmed = en.trim().toUpperCase();
+      /**
+       * If it is a number
+       */
+      if (Number(trimmed)) {
+        switch ((Number(trimmed) >= 0) ? 1 : -1) {
+          case 1: {
+            console.log('Positive Shift');
+            list = this.shiftRight(Number(trimmed), list)
+
+            break
+          }
+          case -1: {
+            console.log('Negative Shift');
+            list = this.shiftLeft(Number(trimmed), list)
+
+            break
+          }
+        }
+        return false;
+      }
+
+      /**
+       * Handle Horizontal/Vertical Flips
+       */
+      switch (trimmed) {
+        case ('H'): {
+          console.log('Horizontal Flip');
+          list = this.horizontalFlip(list)
+          break
+        }
+        case ('V'): {
+          console.log('Vertical Flip');
+          list = this.verticalFilp(list)
+          break
+        }
+        default: {
+          console.warn('Invalid Code:', trimmed);
+        }
+      }
+    })
+    // Final encoded string
+    return list.join('');
+  }
+
+  /**
+   * Map Text To Encodeing
+   */
+  mapText(encodedString, text, defaultList) {
+    const mappedArray = text.split('').map(t => {
+      const place = defaultList.indexOf(t.toLowerCase())
+      if (place < 0) {
+        // Invalid character
+        return t;
+      }
+      // Encoded character
+      return encodedString[place];
+    })
+    return mappedArray.join('');
+  }
+
+  /**
+   * Shift Left - TODO: still needs fixing
+   */
+  shiftLeft(places, list) {
+    const shift = places *= -1;
+    return list.map(d => {
+      const asArray = d.split('');
+      asArray.unshift.apply(asArray, asArray.splice(0, shift));
+
+      return asArray.join('')
+    })
+  }
+
+  /**
+   * Shift Right
+   */
+  shiftRight(places, list) {
+    return list.map(d => {
+      const asArray = d.split('');
+      asArray.push.apply(asArray, asArray.splice(0, places));
+
+      return asArray.join('')
+    })
+  }
+
+  /**
+   * Horizontal Flip
+   */
+  horizontalFlip(list) {
+    return list.map(d => {
+      return d.split('').reverse().join('')
+    });
+  }
+
+  /**
+   * Vertical Flip
+   */
+  verticalFilp(list) {
+    return list.reverse()
+  }
+}
